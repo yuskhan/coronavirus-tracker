40fa240c5ef08c11618658073467cdf099adfc32
Yusuf Khan <yusuf.khan1000098@gmail.com>
2020-07-16 18:13:11 -0400

    Updates

---
+++ precache-manifest.42193b8f552fb7132124c3af95e546da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac1b16f29a8ae3e7ca5948b7ead722a9",
+    "url": "/coronavirus-tracker/index.html"
+  },
+  {
+    "revision": "738ab916fab70b72e84f",
+    "url": "/coronavirus-tracker/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "304e99aa8bf024166a11",
+    "url": "/coronavirus-tracker/static/css/main.8b419529.chunk.css"
+  },
+  {
+    "revision": "738ab916fab70b72e84f",
+    "url": "/coronavirus-tracker/static/js/2.917844b2.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/coronavirus-tracker/static/js/2.917844b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "304e99aa8bf024166a11",
+    "url": "/coronavirus-tracker/static/js/main.65bc8fde.chunk.js"
+  },
+  {
+    "revision": "f2a1110340dd5f6b5c37",
+    "url": "/coronavirus-tracker/static/js/runtime-main.f666537b.js"
+  }
+]);
